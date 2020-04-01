bbc82d1b8dc3cc04480a88cd149f1149e51791a7
Tom Sandalon <tomsand@post.bgu.ac.il>
2020-04-01 18:56:38 +0300

    part 2 question 3

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -1,4 +1,4 @@
-import  {filter, map} from "ramda"
+import  {filter, map, reduce, compose} from "ramda"
 
 
 /* Question 1 */
@@ -13,7 +13,10 @@ export const mapMat : <T> (func: (obj:T)=>T, arr:T[][]) => T[][] =
     <T>(func: (obj:T)=>T, arr:T[][]) => map((x:T[])=>map(func, x), arr);
 
 /* Question 3 */
-export const composeMany = undefined;
+export const composeMany : <T> (array:Array<(obj:T)=>T>) => ((newFunc:T)=>T) = <T> (array:Array<(obj:T)=>T>) =>
+    reduce((acc,curr)=> compose(acc, curr), (x:T)=>x, array);
+
+
 
 /* Question 4 */
 interface Languages {
--- test/part2.test.ts
+++ test/part2.test.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { partition, mapMat } from "../src/part2/part2";
+import { partition, mapMat, composeMany } from "../src/part2/part2";
 
 describe("Assignment 1 Part 2", () => {
     it("check partition", ()=> {
@@ -10,5 +10,8 @@ describe("Assignment 1 Part 2", () => {
         expect(mapMat<number>((x)=>x*x,[[1,2,3],[4,5,6],[7,8,9]])).to.deep.equals( [[ 1, 4, 9 ], [ 16, 25, 36 ], [ 49, 64, 81 ]]);
     });
 
-
+    it("check composMany", ()=> {
+        const squareAndHalf = composeMany([(x:number)=>x*4,(x: number) => x / 2, (x: number) => x * x]);
+        expect(squareAndHalf(5)).to.equals(50);
+    });
 });
