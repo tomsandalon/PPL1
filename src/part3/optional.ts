d8fbbf1c7f909cffadcaf7b66a31323f57783bb8
Tom <tomsand@post.bgu.ac.il>
2020-04-04 13:18:07 +0300

    finished with optional

--- src/part3/optional.ts
+++ src/part3/optional.ts
@@ -1,12 +1,23 @@
 /* Question 1 */
 
-export type Optional<T> = undefined;
+export type Optional<T>= {
+    tag: 'Some';
+    value: T
+}|{
+    tag: 'None';
+}
 
-export const makeSome = undefined;
-export const makeNone = undefined;
+export const makeSome : <T>(value:T)=>Optional<T> = <T>(insertValue:T) => {
+    return {tag:'Some',value:insertValue};
+};
+export const makeNone : <T>()=>Optional<T> = <T>()=>{
+    return {tag:'None'};
+};
 
-export const isSome = undefined;
-export const isNone = undefined;
+export const isSome : <T>(o:Optional<T>) => boolean = <T>(o:Optional<T>) => o.tag === 'Some';
+export const isNone : <T>(o:Optional<T>) => boolean = <T>(o:Optional<T>) => o.tag === 'None';
 
 /* Question 2 */
-export const bind = undefined;
+export const bind : <T, U>(optional: Optional<T>, f: (x: T) => Optional<U>) => Optional<U> = 
+<T,U> (optional: Optional<T>, f: (x: T) => Optional<U>) =>
+(optional.tag === 'Some') ? f(optional.value) : {tag:'None'};
--- test/part3.test.ts
+++ test/part3.test.ts
@@ -1,5 +1,27 @@
 import { expect } from "chai";
+import { Optional, makeNone, makeSome, isSome, isNone, bind } from "../src/part3/optional";
+import { partition } from "ramda";
+
+const safeDiv = (x: number, y: number): Optional<number> =>
+y === 0 ? makeNone() : makeSome(x / y);
+const minusOne = (x: number): Optional<number> => makeSome(x-1);
+const divideByZero = (x: number): Optional<number> => makeNone();
+
 
 describe("Assignment 1 Part 3", () => {
-
+    it("check Optional q1.1", ()=> {
+        expect(safeDiv(5,2)).to.deep.equals({ tag: 'Some', value: 2.5 });
+        expect(safeDiv(5,0)).to.deep.equals({ tag: 'None' });
+    });
+    it("check Optional q1.2", ()=> {
+        expect(isSome(safeDiv(5,2))).to.equals(true);
+        expect(isNone(safeDiv(5,0))).to.equals(true);
+    });
+    it("check Optional q2", ()=> {
+        expect(bind({tag:'Some',value:5}, minusOne)).to.deep.equals({ tag: 'Some', value: 4 });
+        expect(bind({tag:'Some',value:5}, divideByZero)).to.deep.equals({ tag: 'None'});
+    });
 });
+
+
+
